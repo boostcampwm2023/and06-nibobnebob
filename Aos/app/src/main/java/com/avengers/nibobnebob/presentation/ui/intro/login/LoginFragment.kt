2137098431c63537401d661ac8af0e7547495b01
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-14 13:53:12 +0900

    #6 feat : LoginFragment 생성

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginFragment.kt
@@ -0,0 +1,14 @@
+package com.avengers.nibobnebob.presentation.ui.intro.login
+
+import android.os.Bundle
+import android.view.View
+import com.avengers.nibobnebob.R
+import com.avengers.nibobnebob.databinding.FragmentLoginBinding
+import com.avengers.nibobnebob.presentation.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.avengers.nibobnebob.presentation.ui.intro.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class LoginViewModel : ViewModel() {
+}
