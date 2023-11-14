655bb74d54c4888283189bc62c1723ce25da8d87
박진성 <plashdof@gmail.com>
2023-11-14 11:51:34 +0900

    #4 chore : AccessTokenInterceptor, BearerInterceptor 셋팅

--- Aos/app/src/main/java/com/avengers/nibobnebob/app/di/NetworkModule.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/app/di/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.avengers.nibobnebob.app.di
 
 import com.avengers.nibobnebob.BuildConfig
+import com.avengers.nibobnebob.config.AccessTokenInterceptor
+import com.avengers.nibobnebob.config.BearerInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,7 +12,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -26,6 +27,8 @@ object NetworkModule {
             .readTimeout(3000, TimeUnit.MILLISECONDS)
             .connectTimeout(3000, TimeUnit.MILLISECONDS)
             .addInterceptor(httpLoggingInterceptor)
+            .addNetworkInterceptor(AccessTokenInterceptor())
+            .addInterceptor(BearerInterceptor())
             .build()
     }
 
