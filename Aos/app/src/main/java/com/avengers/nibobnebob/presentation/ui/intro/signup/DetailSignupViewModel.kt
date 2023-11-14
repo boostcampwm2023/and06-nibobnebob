00696718a413066dfb148807ca9791ffcb524e3f
박진성 <plashdof@gmail.com>
2023-11-14 18:50:49 +0900

    #16 feat : DetailSignup Fragment, ViewModel, layout 생성

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupViewModel.kt
@@ -0,0 +1,9 @@
+package com.avengers.nibobnebob.presentation.ui.intro.signup
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailSignupViewModel @Inject constructor(): ViewModel() {
+}
