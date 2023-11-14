83e46cf0d7d30cf0dc5ad8eb515acddd7f261473
박진성 <plashdof@gmail.com>
2023-11-14 11:30:23 +0900

    #4 chore : presentation-ui package 구조화.

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/MainActivity.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.avengers.nibobnebob
+package com.avengers.nibobnebob.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avengers.nibobnebob.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
