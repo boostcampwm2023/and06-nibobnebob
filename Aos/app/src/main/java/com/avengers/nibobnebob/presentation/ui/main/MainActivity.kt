3710c54090872574a65635824d0b75310e313d6b
young <syoh0313@yonsei.ac.kr>
2023-11-14 17:06:00 +0900

    #20 feat : BottomNavigation 및 화면전환 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/MainActivity.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.avengers.nibobnebob.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.avengers.nibobnebob.R
+import com.avengers.nibobnebob.databinding.ActivityMainBinding
+import com.avengers.nibobnebob.presentation.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNavBar.setupWithNavController(navController)
     }
+
 }
