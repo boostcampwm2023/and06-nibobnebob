005f746335c1e2d4f8d027bd6bfc76f457c0c2a3
박진성 <plashdof@gmail.com>
2023-11-14 20:19:44 +0900

    #16 feat : DetailSignup UiState 정의

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupViewModel.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupViewModel.kt
@@ -2,8 +2,26 @@ package com.avengers.nibobnebob.presentation.ui.intro.signup
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+
+data class DetailSignupUiState(
+    val nickState: InputState = InputState.Empty,
+    val birthState: InputState = InputState.Empty,
+)
+
+sealed class InputState{
+    data object Empty: InputState()
+    data class Success(val msg: String): InputState()
+    data class Error(val msg: String): InputState()
+}
 
 @HiltViewModel
 class DetailSignupViewModel @Inject constructor(): ViewModel() {
+
+    private val _uiState = MutableStateFlow(DetailSignupUiState())
+    val uiState: StateFlow<DetailSignupUiState> = _uiState.asStateFlow()
+
 }
