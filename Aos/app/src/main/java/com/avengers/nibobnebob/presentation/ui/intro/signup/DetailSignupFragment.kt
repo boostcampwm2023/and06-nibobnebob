46fbe38c101ab74804509f28a96976059ab3d5f7
박진성 <plashdof@gmail.com>
2023-11-14 20:55:37 +0900

    #16 feat : Custom helperMessage, TextInputLayout 테두리색 Binding Adapter 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupFragment.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupFragment.kt
@@ -2,12 +2,19 @@ package com.avengers.nibobnebob.presentation.ui.intro.signup
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.viewModels
 import com.avengers.nibobnebob.R
 import com.avengers.nibobnebob.databinding.FragmentDetailSignupBinding
 import com.avengers.nibobnebob.presentation.base.BaseFragment
+import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
 
-class DetailSignupFragment: BaseFragment<FragmentDetailSignupBinding>(R.layout.fragment_detail_signup) {
+@AndroidEntryPoint
+class DetailSignupFragment :
+    BaseFragment<FragmentDetailSignupBinding>(R.layout.fragment_detail_signup) {
 
     private val viewModel: DetailSignupViewModel by viewModels()
 
@@ -15,5 +22,33 @@ class DetailSignupFragment: BaseFragment<FragmentDetailSignupBinding>(R.layout.f
         super.onViewCreated(view, savedInstanceState)
 
         binding.vm = viewModel
+    }
+}
+
+@BindingAdapter("helperMessage")
+fun bindHelpMessage(view: TextView, inputState: InputState) {
+    when (inputState) {
+        is InputState.Success -> {
+            view.text = inputState.msg
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.nn_primary6))
+        }
+
+        is InputState.Error -> {
+            view.text = inputState.msg
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.nn_rainbow_red))
+        }
+
+        else -> {
+            view.text = ""
+        }
+    }
+}
+
+@BindingAdapter("textLayoutColor")
+fun bindTextLayoutColor(view: TextInputLayout, inputState: InputState) {
+    when (inputState) {
+        is InputState.Success -> view.boxStrokeColor = ContextCompat.getColor(view.context, R.color.nn_primary6)
+        is InputState.Error -> view.boxStrokeColor = ContextCompat.getColor(view.context, R.color.nn_rainbow_red)
+        else -> view.boxStrokeColor = ContextCompat.getColor(view.context, R.color.nn_primary6)
     }
 }
