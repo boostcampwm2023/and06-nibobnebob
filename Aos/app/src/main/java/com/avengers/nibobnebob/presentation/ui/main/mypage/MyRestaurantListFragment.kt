d7a0aee7573d5a9d348edc55be1d6a2c6577e9cf
young <syoh0313@yonsei.ac.kr>
2023-11-15 00:28:39 +0900

    #18 feat : mypage 메인에서 각 상세 페이지로 이동 구현

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyRestaurantListFragment.kt
@@ -0,0 +1,19 @@
+package com.avengers.nibobnebob.presentation.ui.main.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.avengers.nibobnebob.R
+import com.avengers.nibobnebob.databinding.FragmentEditProfileBinding
+import com.avengers.nibobnebob.databinding.FragmentMyRestaurantListBinding
+import com.avengers.nibobnebob.presentation.base.BaseFragment
+
+class MyRestaurantListFragment : BaseFragment<FragmentMyRestaurantListBinding>(R.layout.fragment_my_restaurant_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/WishRestaurantListFragment.kt
@@ -0,0 +1,19 @@
+package com.avengers.nibobnebob.presentation.ui.main.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.avengers.nibobnebob.R
+import com.avengers.nibobnebob.databinding.FragmentEditProfileBinding
+import com.avengers.nibobnebob.databinding.FragmentWishRestaurantListBinding
+import com.avengers.nibobnebob.presentation.base.BaseFragment
+
+class WishRestaurantListFragment : BaseFragment<FragmentWishRestaurantListBinding>(R.layout.fragment_wish_restaurant_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
