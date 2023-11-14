c57acc3ce66a31057004d4f2d74a57f319321ee2
박진성 <plashdof@gmail.com>
2023-11-14 20:21:07 +0900

    #16 feat : DetailSignup Events 정의

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupViewModel.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/signup/DetailSignupViewModel.kt
@@ -2,8 +2,11 @@ package com.avengers.nibobnebob.presentation.ui.intro.signup
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
@@ -18,10 +21,18 @@ sealed class InputState{
     data class Error(val msg: String): InputState()
 }
 
+sealed class DetailSignupEvents{
+    data object NavigateToBack : DetailSignupEvents()
+    data object NavigateToMainActivity: DetailSignupEvents()
+}
+
 @HiltViewModel
 class DetailSignupViewModel @Inject constructor(): ViewModel() {
 
     private val _uiState = MutableStateFlow(DetailSignupUiState())
     val uiState: StateFlow<DetailSignupUiState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<DetailSignupEvents>()
+    val events: SharedFlow<DetailSignupEvents> = _events.asSharedFlow()
 
 }
