b084270577cab077ce6ffd859bfc30cfc976b2aa
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-15 13:16:40 +0900

    #6 feat : 로그인 관련 뷰모델 추가, api 대기

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginFragment.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.avengers.nibobnebob.BuildConfig
 import com.avengers.nibobnebob.R
 import com.avengers.nibobnebob.databinding.FragmentLoginBinding
@@ -11,29 +12,49 @@ import com.avengers.nibobnebob.presentation.base.BaseFragment
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.OAuthLoginCallback
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
     private val TAG = "LoginFragmentDebug"
     private val viewModel : LoginViewModel by viewModels()
-    private var email : String = ""
-    private var pic : String = ""
-    private var token : String = ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        NaverIdLoginSDK.initialize(requireContext(), BuildConfig.NAVER_LOGIN_CLIENT_ID, BuildConfig.NAVER_LOGIN_CLIENT_SECRET, "gyroh")
+        binding.vm = viewModel
+
+        NaverIdLoginSDK.initialize(
+            requireContext(),
+            BuildConfig.NAVER_LOGIN_CLIENT_ID,
+            BuildConfig.NAVER_LOGIN_CLIENT_SECRET, TAG)
         NaverIdLoginSDK.showDevelopersLog(true)
 
+
+        initEventObserver()
         binding.btnNaver.setOnClickListener {
             naverLogin()
         }
-        binding.logoutTest.setOnClickListener {
-            naverLogout()
+
+    }
+
+    private fun initEventObserver(){
+        repeatOnStarted {
+            viewModel.eventFlow.collect{
+                when(it){
+                    LoginViewModel.LoginEvent.LoginSuccess -> {
+                        //회원가입으로 이동
+                    }
+                    LoginViewModel.LoginEvent.LoginFailure -> {
+                        //다이얼로그 띄우기
+                    }
+                }
+            }
         }
     }
+
 
     private fun naverLogin(){
         val oAuthLoginCallback = object : OAuthLoginCallback{
@@ -44,11 +65,13 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
                 val errorCode = NaverIdLoginSDK.getLastErrorCode().code
                 val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
                 Log.d(TAG,"errorCode:$errorCode, errorDesc:$errorDescription")
+                //TODO : FINAL -> 실패 다이얼로그 띄워주기
             }
 
             override fun onSuccess() {
-                Log.d(TAG,NaverIdLoginSDK.getAccessToken().toString())
-                binding.loginTest.text = NaverIdLoginSDK.getAccessToken().toString()
+                viewModel.token.value = NaverIdLoginSDK.getAccessToken().toString()
+                viewModel.postNaverLogin()
+                // TODO : datastore이든 sharedpref이든 서버와의 통신 진행 후 토큰을 저장 해야함 (repository에서, 추가한 preferencesdatastore에 진행)
             }
         }
         NaverIdLoginSDK.authenticate(requireContext(), oAuthLoginCallback)
--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginViewModel.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/intro/login/LoginViewModel.kt
@@ -1,7 +1,100 @@
 package com.avengers.nibobnebob.presentation.ui.intro.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-//@HiltViewModel
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+    private val TAG = "LoginViewModelDebug"
+
+    private val _eventFlow = MutableSharedFlow<LoginEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private val _uiState = MutableStateFlow(CommonRequest())
+    val uiState = _uiState.asStateFlow()
+
+    val email = MutableStateFlow("")
+    val password = MutableStateFlow("")
+    val token = MutableStateFlow("")
+
+    init {
+        observeEmail()
+        observePassword()
+    }
+
+    private fun observeEmail(){
+        email.onEach { newEmail ->
+            _uiState.update { state ->
+                state.copy(email = newEmail)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun observePassword() {
+        password.onEach { newPassword ->
+            _uiState.update { state ->
+                state.copy(password = newPassword)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    fun postCommonLogin(){
+        Log.d(TAG,_uiState.value.toString())
+        viewModelScope.launch {
+            //TODO : flow
+            //repository -> 통신 -> flow로
+        }
+    }
+
+    fun postNaverLogin(){
+        viewModelScope.launch {
+            //네이버 통신 받아온것을 이제 서버와 통신 로그인 이벤트를 성공시 -> 메인 이벤트를 eventflow로 진행?
+            //통신 -> 토큰 사용 -> 서버와의 네이버 로그인 진행 예정
+        }
+    }
+
+    sealed class LoginEvent {
+        data object LoginSuccess : LoginEvent()
+        data object LoginFailure : LoginEvent()
+    }
+
+
 }
+
+/*
+interface ApiService {
+    @POST("login")
+    suspend fun postLogin(@Body info: Info): Flow<로그인정보>
+}
+
+    class 로그인repository(private val apiService: 인포 서비스) {
+        apiService.postLogin(info)
+        .map { response ->
+            // 여기서 음 그 뭐야 여기서 이제 eventflow처리?
+            response.body() ?: throw SomeException("Response body is null")
+        }
+        .onStart {
+            // 로딩 상태중에 로딩이란걸 보여주는 처리를 한다면?
+        }
+        .catch { e ->
+            // 에러 핼들링
+            throw when (e) {
+                is HttpException -> AnotherException("HTTP Error: ${e.code()}")
+                else -> e
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+
+ */
