30df76f17ad8c50e9c16178fab46a3ec97ee998a
young <syoh0313@yonsei.ac.kr>
2023-11-15 17:43:46 +0900

    #18 feat : MyPage uiState 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyPageFragment.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyPageFragment.kt
@@ -2,14 +2,20 @@ package com.avengers.nibobnebob.presentation.ui.main.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.avengers.nibobnebob.R
 import com.avengers.nibobnebob.databinding.FragmentMyPageBinding
 import com.avengers.nibobnebob.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     private lateinit var navController: NavController
+    private val viewModel : MyPageViewModel by viewModels()
+    private val sharedViewModel : MyPageSharedViewModel by viewModels ()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -17,24 +23,32 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
     }
 
     private fun initView(view : View){
+        binding.vm = sharedViewModel
         navController = Navigation.findNavController(view)
-        with(binding) {
-            vEditProfile.setOnClickListener {
-                navController.navigate(
-                    MyPageFragmentDirections.actionMyPageFragmentToEditProfileFragment()
-                )
-            }
-            vMyList.setOnClickListener {
-                navController.navigate(
-                    MyPageFragmentDirections.actionMyPageFragmentToMyRestaurantListFragment()
-                )
-            }
-            vWishList.setOnClickListener {
-                navController.navigate(
-                    MyPageFragmentDirections.actionMyPageFragmentToWishRestaurantListFragment()
-                )
+
+
+        viewLifecycleOwner.repeatOnStarted {
+            sharedViewModel.uiEvent.collect{ event ->
+                when(event){
+                    is MyPageUiEvent.NavigateToEditProfile ->
+                        navController.navigate(MyPageFragmentDirections.globalToEditProfileFragment())
+                    is MyPageUiEvent.NavigateToMyList ->
+                        navController.navigate(MyPageFragmentDirections.globalToMyRestaurantListFragment())
+                    is MyPageUiEvent.NavigateToWishList ->
+                        navController.navigate(MyPageFragmentDirections.globalToWishRestaurantListFragment())
+                    else -> Unit
+                }
+
             }
         }
+
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.uiState.collectLatest { state ->
+                binding.uiState = state
+            }
+        }
+
+
 
     }
 
---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyPageViewModel.kt
@@ -0,0 +1,41 @@
+package com.avengers.nibobnebob.presentation.ui.main.mypage
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(MyPageUiState())
+    val uiState: StateFlow<MyPageUiState> = _uiState.asStateFlow()
+
+    init {
+        getUserInfo()
+    }
+
+
+    data class User(
+        val nickName: String,
+        val age: String,
+        val area: String
+    )
+
+    private fun getUserInfo() {
+        // data layer 에서 가져왔다고 가정
+        val user = User("tester", "20대", "용산구")
+        flow { emit(user) }.onEach {
+            _uiState.update { state ->
+                state.copy(nickName = it.nickName, age = it.age, area = it.area)
+            }
+        }.launchIn(viewModelScope)
+
+    }
+}
