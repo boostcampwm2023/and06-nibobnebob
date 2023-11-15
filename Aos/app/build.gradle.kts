f49f494cc33fce7af92946d8484c750cad5c2c3c
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-15 12:16:29 +0900

    #6 chore : preferences datastore 의존성 추가

--- Aos/app/build.gradle.kts
+++ Aos/app/build.gradle.kts
@@ -97,4 +97,8 @@ dependencies {
 
     //naver Login
     implementation ("com.navercorp.nid:oauth:5.8.0")
+
+    //datastore preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-core:1.0.0")
 }
