a6a8b5fe3e56b12aae1e4837dc246559528a724e
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-15 21:18:51 +0900

    #3 feat : 자동 로그인 여부 확인 후 main,intro 결정
    자동로그인 여부 (향후 로그인 api 연동시 진행 datastore진행 예정) 확인 후
    자동로그인 x -> intro
    자동로그인 o -> accesstoken 서버 확인 후 있다면 main 없으면 갱신 후 main의 로직으로 진행

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/splash/SplashActivity.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/splash/SplashActivity.kt
@@ -1,4 +1,60 @@
 package com.avengers.nibobnebob.presentation.ui.splash
 
-class SplashActivity {
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.avengers.nibobnebob.databinding.ActivitySplashBinding
+import com.avengers.nibobnebob.presentation.base.BaseActivity
+import com.avengers.nibobnebob.presentation.ui.intro.IntroActivity
+import com.avengers.nibobnebob.presentation.ui.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(2000)
+            viewModel.getAutoLogin()
+        }
+
+        lifecycleScope.launch {
+            viewModel.eventFlow.collect{
+                when(it){
+                    is SplashViewModel.NavigationEvent.NavigateToIntro -> moveToIntroActivity()
+                    is SplashViewModel.NavigationEvent.NavigateToMain -> moveToMainActivity()
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+    }
+
+    private fun moveToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun moveToIntroActivity() {
+        val intent = Intent(this, IntroActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
