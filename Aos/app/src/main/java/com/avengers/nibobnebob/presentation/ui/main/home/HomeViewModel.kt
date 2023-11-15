49a0edf7132874b3e1569bde5f37a0211f7bf570
박진성 <plashdof@gmail.com>
2023-11-16 00:43:26 +0900

    #30 feat : Tracking Mode 관련 UiState 및 UiState 변경함수 구현

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/home/HomeViewModel.kt
@@ -0,0 +1,53 @@
+package com.avengers.nibobnebob.presentation.ui.main.home
+
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+data class HomeUiState(
+    val locationTrackingState: TrackingState = TrackingState.On
+)
+
+sealed class TrackingState {
+    data object TryOn : TrackingState()
+    data object On : TrackingState()
+    data object Off : TrackingState()
+}
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    fun locationBtnClicked() {
+        _uiState.update { state ->
+            state.copy(
+                locationTrackingState = if (_uiState.value.locationTrackingState == TrackingState.On) TrackingState.TryOn
+                else TrackingState.Off
+            )
+        }
+    }
+
+    fun trackingOn() {
+        _uiState.update { state ->
+            state.copy(
+                locationTrackingState = TrackingState.On
+            )
+        }
+    }
+
+    fun trackingOff() {
+        _uiState.update { state ->
+            state.copy(
+                locationTrackingState = TrackingState.Off
+            )
+        }
+    }
+
+}
