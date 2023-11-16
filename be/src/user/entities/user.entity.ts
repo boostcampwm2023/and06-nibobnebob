9287f9b976f826de4026a1308cf2cd45b3c9f488
GeunH <unlxrmsgur@naver.com>
2023-11-16 15:16:00 +0900

    feat : BaseResponse Interceptor 적용 #35
    - baseresponse.ts 생성
    - error.filter.ts 생성
    - reponse.interceptor.ts 생성
    
    Co-authored-by: LeeTH916 <thoon916@naver.com>

--- be/src/user/entities/user.entity.ts
+++ be/src/user/entities/user.entity.ts
@@ -1,42 +1,41 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    DeleteDateColumn
-  } from 'typeorm';
-  
-  @Entity()
-  export class User {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
-  
-    @Column({ type: 'varchar', length: 20, unique : true })
-    nickName: string;
-  
-    @Column({ type: 'varchar', length: 50 })
-    email: string;
-  
-    @Column({ type: 'int' })
-    age: number;
-  
-    @Column({ type: 'boolean' })
-    gender: boolean;
-  
-    @Column({ type: 'varchar', length: 50, nullable: true })
-    password: string | null;
-  
-    @Column({ type: 'varchar', length: 20, nullable: true })
-    social_provider: string | null;
-  
-    @CreateDateColumn({ type: 'timestamp' })
-    created_at: Date;
-  
-    @DeleteDateColumn({ type: 'timestamp', nullable: true})
-    deleted_at: Date | null;
-  
-    @UpdateDateColumn({ type: 'timestamp'})
-    updated_at: Date;
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column({ type: "varchar", length: 20, unique: true })
+  nickName: string;
+
+  @Column({ type: "varchar", length: 50 })
+  email: string;
+
+  @Column({ type: "int" })
+  age: number;
+
+  @Column({ type: "boolean" })
+  gender: boolean;
+
+  @Column({ type: "varchar", length: 50, nullable: true })
+  password: string | null;
+
+  @Column({ type: "varchar", length: 20, nullable: true })
+  social_provider: string | null;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @DeleteDateColumn({ type: "timestamp", nullable: true })
+  deleted_at: Date | null;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updated_at: Date;
+}
--- be/src/user/user.service.ts
+++ be/src/user/user.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { UserInfoDto } from "./dto/userInfo.dto";
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private usersRepository: UserRepository,
-    ) { }
-    signup(userInfoDto: UserInfoDto) {
-        return this.usersRepository.createUser(userInfoDto);
-    }
+  constructor(
+    @InjectRepository(UserRepository)
+    private usersRepository: UserRepository
+  ) {}
+  signup(userInfoDto: UserInfoDto) {
+    return this.usersRepository.createUser(userInfoDto);
+  }
 }
