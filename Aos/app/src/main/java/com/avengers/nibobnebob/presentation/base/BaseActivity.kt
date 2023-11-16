a97d95feedf4f41d604256abf797d1fb7bc6b2cd
박진성 <plashdof@gmail.com>
2023-11-16 13:35:21 +0900

    #3 feat : BaseActivity 에서 networkstate 감지후 Disconnect 시 snackbar 띄우기
    Co-authored-by: yy0ung <saeyoung1192@gmail.com>
    Co-authored-by: BENDENG1 <nogom369@gmail.com>

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/base/BaseActivity.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/base/BaseActivity.kt
@@ -8,12 +8,17 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.avengers.nibobnebob.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-abstract class BaseActivity<B : ViewDataBinding>(private val inflate: (LayoutInflater) -> B) :
-    AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(
+    private val inflate: (LayoutInflater) -> B,
+) : AppCompatActivity() {
+    
     protected lateinit var binding: B
+    protected abstract val activityViewModel : BaseActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +31,30 @@ abstract class BaseActivity<B : ViewDataBinding>(private val inflate: (LayoutInf
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
         }
+    }
+    
+    protected fun initNetworkStateObserver(){
+        repeatOnStarted {
+            activityViewModel.networkState.collect{
+                when(it){
+                    NetWorkState.NETWORK_DISCONNECTED -> noNetworkSnackBar()
+                    NetWorkState.NETWORK_CONNECTED -> {
+                        // todo 네트워크 연결 감지시 fra
+                    } 
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    private fun noNetworkSnackBar() {
+        Snackbar.make(
+            binding.root,
+            R.string.no_network_text,
+            Snackbar.LENGTH_INDEFINITE
+        ).setAction(R.string.retry) {
+            //initViewData()
+        }.show()
     }
 
 }
