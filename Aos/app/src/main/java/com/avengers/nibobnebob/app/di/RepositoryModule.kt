9edf57e118873ac3c8a2a137088d92320b4bd6ac
박진성 <plashdof@gmail.com>
2023-11-17 02:36:27 +0900

    #17 feat : intro RepositoryModule Setting

--- Aos/app/src/main/java/com/avengers/nibobnebob/app/di/RepositoryModule.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/app/di/RepositoryModule.kt
@@ -1,19 +1,23 @@
 package com.avengers.nibobnebob.app.di
 
+import com.avengers.nibobnebob.data.repository.IntroRepository
+import com.avengers.nibobnebob.data.repository.IntroRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
-//    @Singleton
-//    @Binds
-//    abstract fun bindImageRepository(
-//        imageRepositoryImpl: ImageRepositoryImpl
-//    ): ImageRepository
+    @Singleton
+    @Binds
+    abstract fun bindImageRepository(
+        introRepositoryImpl: IntroRepositoryImpl
+    ): IntroRepository
 
 }
 
