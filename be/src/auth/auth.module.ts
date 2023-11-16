0ea56050cbae329774bcc6dbae08341c2c1bf2a2
LeeTH916 <thoon916@naver.com>
2023-11-16 20:05:23 +0900

    feat : passport 모듈의 jwt 인증 전락 구현 #6
    - passport 모듈 등록
    - passport 모듈의 기본 전략을 jwt로 설정
    - jwt 전략에서 정상적인 토큰인 경우 해당 user의 nickName을 반환

--- be/src/auth/auth.module.ts
+++ be/src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports:[
+    PassportModule.register({defaultStrategy: 'jwt'}),
     JwtModule.register({
       secret: "nibobnebob",
       signOptions: {
@@ -15,6 +17,7 @@ import { UserModule } from 'src/user/user.module';
     UserModule
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [PassportModule]
 })
 export class AuthModule {}
---
+++ be/src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserRepository } from 'src/user/user.repository';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private userRepository: UserRepository
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "nibobnebob",
+    });
+  }
+
+  async validate(payload: any) {
+    const {nickName} = payload
+
+    return {nickName};
+
+  }
+}
