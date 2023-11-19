d8a3b0488158a0803dc5a158242819f49117b9b2
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-19 20:50:25 +0900

    #6 feat : AccessTokenInterceptor 세팅
    jwt 통신을 위해 AccessToken 추가
    TODO : 서버의 Refresh에 대한 논의 진행 후 진행해야함

--- Aos/app/src/main/java/com/avengers/nibobnebob/config/AccessTokenInterceptor.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/config/AccessTokenInterceptor.kt
@@ -1,22 +1,33 @@
 package com.avengers.nibobnebob.config
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.avengers.nibobnebob.presentation.util.Constants.ACCESS
+import com.avengers.nibobnebob.presentation.util.Constants.ACCESSTOKEN
+import com.avengers.nibobnebob.presentation.util.Constants.AUTHORIZATION
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
+import javax.inject.Inject
 
-class AccessTokenInterceptor(): Interceptor {
+class AccessTokenInterceptor @Inject constructor(private val dataStore : DataStore<Preferences>): Interceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        val builder: Request.Builder = chain.request().newBuilder()
-//        val jwt: String? = sharedPreferences.getString(X_ACCESS_TOKEN, null)
-//        jwt?.let {
-//            builder.addHeader("Authorization", jwt)
-//        } ?: run {
-//
-//        }
+            val key = stringPreferencesKey(ACCESSTOKEN)
 
+            val accessToken =  runBlocking {
+                val preferences = dataStore.data.first()
+                preferences[key]
+            }
+        val builder: Request.Builder = chain.request().newBuilder()
+        accessToken?.let {
+            builder.addHeader(AUTHORIZATION,"$ACCESS $accessToken")
+        }
         return chain.proceed(builder.build())
     }
 }
--- Aos/app/src/main/java/com/avengers/nibobnebob/config/BearerInterceptor.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/config/BearerInterceptor.kt
@@ -4,8 +4,9 @@ package com.avengers.nibobnebob.config
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
+import javax.inject.Inject
 
-class BearerInterceptor : Interceptor {
+class BearerInterceptor @Inject constructor() : Interceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
