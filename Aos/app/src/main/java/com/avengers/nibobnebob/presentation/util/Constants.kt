2af2207cea971044860eceb33ae8016fc7c8e709
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-19 20:43:23 +0900

    #6 feat : Constants 상수 추가

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/util/Constants.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/util/Constants.kt
@@ -1,7 +1,12 @@
 package com.avengers.nibobnebob.presentation.util
 
 object Constants {
+    const val BASE_URL = "http://223.130.162.237:8000/"
     const val AUTOLOGIN = "AUTOLOGIN"
+    const val ACCESS = "Access"
     const val ACCESSTOKEN = "ACCESSTOKEN"
     const val REFRESHTOKEN = "REFRESHTOKEN"
+    const val AUTHORIZATION = "authorization"
+    const val TRUE = "true"
+    const val FALSE = "false"
 }
