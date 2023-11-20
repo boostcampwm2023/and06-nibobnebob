a96db007e0ebf1e220830b655a72c0c3775c082e
young <syoh0313@yonsei.ac.kr>
2023-11-20 17:04:06 +0900

    #18 feat : Response 로 감싸고 분기 처리 위한 ApiState (Base) 구현

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/model/ApiState.kt
@@ -0,0 +1,7 @@
+package com.avengers.nibobnebob.data.model
+
+sealed class ApiState<out T> {
+    data class Success<out T>(val data: T) : ApiState<T>()
+    data class Error(val statusCode: Int, val message: String) : ApiState<Nothing>()
+    data class Exception(val e: Throwable) : ApiState<Nothing>()
+}
---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/remote/NnApi.kt
@@ -0,0 +1,13 @@
+package com.avengers.nibobnebob.data.remote
+
+import com.avengers.nibobnebob.data.model.response.MyPageInfoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface NnApi {
+    @GET("api/user/details")
+    suspend fun getMyPageInfo(
+        @Header("Authorization") token : String,
+    ) : Response<MyPageInfoResponse>
+}
