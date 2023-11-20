60e5727331fbe576e8fc4edc65edd55a1672dab0
박진성 <plashdof@gmail.com>
2023-11-21 01:00:43 +0900

    #17 feat : ApiRun 제네릭 함수 구현, 보일러 플레이트 제거

--- Aos/app/src/main/java/com/avengers/nibobnebob/data/repository/IntroRepositoryImpl.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/repository/IntroRepositoryImpl.kt
@@ -2,8 +2,8 @@ package com.avengers.nibobnebob.data.repository
 
 import com.avengers.nibobnebob.data.model.ApiState
 import com.avengers.nibobnebob.data.model.request.DetailSignupRequest
+import com.avengers.nibobnebob.data.model.runNNApi
 import com.avengers.nibobnebob.data.remote.IntroAPI
-import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -13,21 +13,7 @@ class IntroRepositoryImpl @Inject constructor(
 ) : IntroRepository {
 
     override fun signup(body: DetailSignupRequest): Flow<ApiState<Unit>> = flow {
-        runCatching { api.signup(body) }
-            .onSuccess { response ->
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        emit(ApiState.Success(it))
-                    } ?: run {
-                        emit(ApiState.Error(0, "응답이 비어있습니다"))
-                    }
-                } else {
-                    val errorData =
-                        Gson().fromJson(response.errorBody()?.string(), ApiState.Error::class.java)
-                    emit(ApiState.Error(errorData.statusCode, errorData.message))
-                }
-            }.onFailure {
-                emit(ApiState.Exception(it))
-            }
+        val result = runNNApi { api.signup(body) }
+        emit(result)
     }
 }
--- Aos/app/src/main/java/com/avengers/nibobnebob/data/repository/ValidationRepositoryImpl.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/repository/ValidationRepositoryImpl.kt
@@ -2,8 +2,8 @@ package com.avengers.nibobnebob.data.repository
 
 import com.avengers.nibobnebob.data.model.ApiState
 import com.avengers.nibobnebob.data.model.response.ValidateResponse
+import com.avengers.nibobnebob.data.model.runNNApi
 import com.avengers.nibobnebob.data.remote.ValidationApi
-import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -13,40 +13,12 @@ class ValidationRepositoryImpl @Inject constructor(
 ) : ValidationRepository {
 
     override fun nickValidation(nickName: String): Flow<ApiState<ValidateResponse>> = flow {
-        runCatching { api.nickValidation(nickName) }
-            .onSuccess { response ->
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        emit(ApiState.Success(it))
-                    } ?: run {
-                        emit(ApiState.Error(0, "응답이 비어있습니다"))
-                    }
-                } else {
-                    val errorData =
-                        Gson().fromJson(response.errorBody()?.string(), ApiState.Error::class.java)
-                    emit(ApiState.Error(errorData.statusCode, errorData.message))
-                }
-            }.onFailure {
-                emit(ApiState.Exception(it))
-            }
+        val result = runNNApi { api.nickValidation(nickName) }
+        emit(result)
     }
 
     override fun emailValidation(email: String): Flow<ApiState<ValidateResponse>> = flow {
-        runCatching { api.emailValidation(email) }
-            .onSuccess { response ->
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        emit(ApiState.Success(it))
-                    } ?: run {
-                        emit(ApiState.Error(0, "응답이 비어있습니다"))
-                    }
-                } else {
-                    val errorData =
-                        Gson().fromJson(response.errorBody()?.string(), ApiState.Error::class.java)
-                    emit(ApiState.Error(errorData.statusCode, errorData.message))
-                }
-            }.onFailure {
-                emit(ApiState.Exception(it))
-            }
+        val result = runNNApi { api.nickValidation(email) }
+        emit(result)
     }
 }
