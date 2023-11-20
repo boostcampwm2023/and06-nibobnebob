840b3617ebc1a6076d5975db37ba8ed2a10ea0c1
GeunH <unlxrmsgur@naver.com>
2023-11-20 21:14:07 +0900

    feat : user restaurant list 엔티티 구현 #33

---
+++ be/src/user/entities/user.restaurantlist.dot.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  PrimaryColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
+import { Review } from 'src/review/entities/review.entity';
+
+@Entity('user_restaurant_lists')
+export class UserRestaurantList {
+  @ManyToOne(() => User)
+  @PrimaryColumn({ name: 'user_id' })
+  userId: User;
+
+  @ManyToOne(() => Restaurant)
+  @PrimaryColumn({ name: 'restaurant_id' })
+  restaurantId: Restaurant;
+
+  @ManyToOne(() => Review)
+  @JoinColumn({ name: 'review_id' })
+  reviewId: Review;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @Column({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
+}
