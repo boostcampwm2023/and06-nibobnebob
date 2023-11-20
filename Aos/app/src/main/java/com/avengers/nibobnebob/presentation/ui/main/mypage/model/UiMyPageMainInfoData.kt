56a46578aa479d528d8cc257ca20e2d874d6d501
young <syoh0313@yonsei.ac.kr>
2023-11-20 17:25:57 +0900

    #18 refactor : mypage UiData 파일 이름 변경

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/model/UiMyPageMainInfoData.kt
@@ -0,0 +1,8 @@
+package com.avengers.nibobnebob.presentation.ui.main.mypage.model
+
+data class UiMyPageInfoData(
+    val nickName : String,
+    val age : String,
+    val location : String,
+    val gender : String,
+)
