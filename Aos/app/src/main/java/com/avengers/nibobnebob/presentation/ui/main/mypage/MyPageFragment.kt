8fac96b9087974d77c31c0edc6f32ef8787c4cda
young <syoh0313@yonsei.ac.kr>
2023-11-24 00:10:32 +0900

    #23 feat : 회원 탈퇴시 dialog 로 다시 한번 확인하기

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyPageFragment.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/mypage/MyPageFragment.kt
@@ -1,25 +1,23 @@
 package com.avengers.nibobnebob.presentation.ui.main.mypage
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.avengers.nibobnebob.R
-import com.avengers.nibobnebob.data.remote.IntroApi
 import com.avengers.nibobnebob.databinding.FragmentMyPageBinding
 import com.avengers.nibobnebob.presentation.base.BaseFragment
 import com.avengers.nibobnebob.presentation.ui.intro.IntroActivity
+import com.avengers.nibobnebob.presentation.ui.main.MainActivity
 import com.avengers.nibobnebob.presentation.ui.main.MainViewModel
 import com.avengers.nibobnebob.presentation.ui.main.mypage.share.MyPageSharedUiEvent
 import com.avengers.nibobnebob.presentation.ui.main.mypage.share.MyPageSharedViewModel
-import com.avengers.nibobnebob.presentation.util.dialogManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
@@ -30,10 +28,10 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initView(view)
+        initView()
     }
 
-    private fun initView(view: View) {
+    private fun initView() {
         binding.svm = sharedViewModel
         binding.vm = viewModel
 
@@ -50,26 +48,30 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
         }
 
         binding.tvWithdraw.setOnClickListener {
-            dialogManager(
-                requireContext(),
-                "정말 탈퇴하시겠습니까?",
-                "모든 정보가 삭제됩니다.",
-                withDrawConfirm(),
-                withDrawDismiss()
-            )
+            AlertDialog.Builder(context)
+                .setTitle("정말 탈퇴하시겠습니까?")
+                .setMessage("모든 정보가 삭제됩니다.")
+                .setPositiveButton("확인"
+                ) { _, _ -> withDrawConfirm() }
+                .setNegativeButton("취소"
+                ) { _, _ -> withDrawDismiss() }
+                .create()
+                .show()
         }
 
 
     }
 
     private fun withDrawConfirm() {
+        Log.d("TEST", "탈퇴 완료")
         viewModel.withdraw()
         repeatOnStarted {
-            viewModel.events.collect{ event ->
-                when(event){
+            viewModel.events.collect { event ->
+                when (event) {
                     is MyEditPageEvent.NavigateToIntro -> {
-            val intent = Intent(context, IntroActivity::class.java)
-            startActivity(intent)
+                        (activity as MainActivity).finish()
+                        val intent = Intent(context, IntroActivity::class.java)
+                        startActivity(intent)
                     }
                 }
             }
