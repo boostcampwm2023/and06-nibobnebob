6f7a310dd2cd5e329e7eed4262a6dd062fe9f68a
GeunH <unlxrmsgur@naver.com>
2023-11-23 16:45:23 +0900

    feat : 내 맛집 리스트 정보 가져오기 api 구현 #63

--- be/src/user/user.module.ts
+++ be/src/user/user.module.ts
@@ -4,10 +4,12 @@ import { UserService } from "./user.service";
 import { UserRepository } from "./user.repository";
 import { AuthModule } from "../auth/auth.module";
 import { forwardRef } from "@nestjs/common";
+import { UserRestaurantListRepository } from "./user.restaurantList.repository";
+
 @Module({
   imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UserRestaurantListRepository],
   exports: [UserRepository],
 })
-export class UserModule {}
+export class UserModule { }
--- be/src/user/user.service.ts
+++ be/src/user/user.service.ts
@@ -4,13 +4,16 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "./user.repository";
 import { TokenInfo } from "./user.decorator";
 import { hashPassword } from "../utils/encryption.utils";
+import { SearchInfoDto } from "../restaurant/dto/seachInfo.dto";
+import { UserRestaurantListRepository } from "./user.restaurantList.repository";
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserRepository)
-    private usersRepository: UserRepository
-  ) {}
+    private usersRepository: UserRepository,
+    private userRestaurantListRepository: UserRestaurantListRepository,
+  ) { }
   async signup(userInfoDto: UserInfoDto) {
     userInfoDto.password = await hashPassword(userInfoDto.password);
     return await this.usersRepository.createUser(userInfoDto);
@@ -29,6 +32,14 @@ export class UserService {
   }
   async getMypageUserDetailInfo(tokenInfo: TokenInfo) {
     return await this.usersRepository.getMypageUserDetailInfo(tokenInfo.id);
+  }
+  async getMyRestaurantListInfo(searchInfoDto: SearchInfoDto, tokenInfo: TokenInfo) {
+    return await this.userRestaurantListRepository
+      .createQueryBuilder('user_restaurant_lists')
+      .leftJoinAndSelect('user_restaurant_lists.restaurant', 'restaurant')
+      .select(['user_restaurant_lists.restaurantId', 'restaurant.name', 'restaurant.location', 'restaurant.address', 'restaurant.category', "restaurant.phoneNumber", "restaurant.reviewCnt"])
+      .where('user_restaurant_lists.user_id = :userId', { userId: tokenInfo.id })
+      .getMany();
   }
   async deleteUserAccount(tokenInfo: TokenInfo) {
     return await this.usersRepository.deleteUserAccount(tokenInfo.id);
