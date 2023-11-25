a6ac3f9c2cf5d90864c38428ec2bcf50b398f28f
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-25 23:38:01 +0900

    #104 feat : 음식점 리뷰 adapter, bindingAdapter구현

---
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/global/restaurantdetail/RestaurantDetailBindingAdapter.kt
@@ -0,0 +1,44 @@
+package com.avengers.nibobnebob.presentation.ui.global.restaurantdetail
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.avengers.nibobnebob.R
+
+
+@BindingAdapter("filterRateBackground")
+fun bindFilterBackground(iv: ImageView, rate : Int){
+    when(rate){
+        1 -> {iv.setBackgroundResource(R.drawable.oval_primary1fill_nostroke)}
+        2 -> {iv.setBackgroundResource(R.drawable.oval_primary2fill_nostroke)}
+        3 -> {iv.setBackgroundResource(R.drawable.oval_primary3fill_nostroke)}
+        4 -> {iv.setBackgroundResource(R.drawable.oval_primary4fill_nostroke)}
+        5 -> {iv.setBackgroundResource(R.drawable.oval_primary5fill_nostroke)}
+    }
+}
+
+@BindingAdapter("thumbsUpStatus")
+fun setThumbsUpStatus(iv: ImageView, isThumbsUp:Boolean){
+    if(isThumbsUp){
+        iv.setImageResource(R.drawable.ic_thumbs_up_fill)
+    }else{
+        iv.setImageResource(R.drawable.ic_thumbs_up_blank)
+    }
+}
+
+@BindingAdapter("thumbsDownStatus")
+fun setThumbsDownStatus(iv: ImageView, isThumbsDown:Boolean){
+    if(isThumbsDown){
+        iv.setImageResource(R.drawable.ic_thumbs_down_fill)
+    }else{
+        iv.setImageResource(R.drawable.ic_thumbs_down_blank)
+    }
+}
+
+@BindingAdapter("wishStatus")
+fun setWishStatus(iv:ImageView, isWish : Boolean){
+    if(isWish){
+        iv.setImageResource(R.drawable.ic_star_full)
+    }else{
+        iv.setImageResource(R.drawable.ic_star_border)
+    }
+}
