509c6edd67ee4d580e351a4917bcc7ccdacae9ec
박진성 <plashdof@gmail.com>
2023-11-27 06:37:15 +0900

    #69 feat : 맛집 추가 총평 글자수 bindingAdapter 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/global/restaurantadd/AddMyRestaurantBindingAdapter.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/global/restaurantadd/AddMyRestaurantBindingAdapter.kt
@@ -1,2 +1,13 @@
 package com.avengers.nibobnebob.presentation.ui.global.restaurantadd
 
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("textLength", "textLimit")
+fun bindTextLength(view: TextView, text: String, limit: Int) {
+    view.text = "(${text.length}/$limit)"
+}
+
