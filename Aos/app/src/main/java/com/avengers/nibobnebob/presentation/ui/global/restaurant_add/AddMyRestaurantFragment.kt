619cacc1086904bfa3b33d9f0e235e9a29b1d50d
박진성 <plashdof@gmail.com>
2023-11-27 06:00:06 +0900

    #69 feat : 맛집 등록 레이아웃 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/global/restaurant_add/AddMyRestaurantFragment.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/global/restaurant_add/AddMyRestaurantFragment.kt
@@ -3,7 +3,6 @@ package com.avengers.nibobnebob.presentation.ui.global.restaurant_add
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.avengers.nibobnebob.R
 import com.avengers.nibobnebob.databinding.FragmentAddMyRestaurantBinding
 import com.avengers.nibobnebob.presentation.base.BaseFragment
@@ -15,5 +14,6 @@ class AddMyRestaurantFragment : BaseFragment<FragmentAddMyRestaurantBinding>(R.l
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
