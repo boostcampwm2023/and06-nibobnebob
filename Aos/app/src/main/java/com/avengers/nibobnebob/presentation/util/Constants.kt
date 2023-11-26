cba3f3cf8657c0b5694ab779901b9005ed6f5deb
박진성 <plashdof@gmail.com>
2023-11-27 03:15:25 +0900

    #86 feat : BASE_URL 도메인 변경

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/util/Constants.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/util/Constants.kt
@@ -2,12 +2,15 @@ package com.avengers.nibobnebob.presentation.util
 
 object Constants {
     const val APP_NAME = "NibobNebob"
-    const val BASE_URL = "http://223.130.162.237:8000/"
+    const val BASE_URL = "https://www.nibobnebob.site:8000/api/"
+
     const val AUTO_LOGIN = "AUTOLOGIN"
     const val ACCESS = "Access"
+
     const val ACCESS_TOKEN = "ACCESSTOKEN"
     const val REFRESH_TOKEN = "REFRESHTOKEN"
     const val NAVER_TOKEN = "NAVERTOKEN"
+
     const val AUTHORIZATION = "authorization"
     const val BEARER = "Bearer"
     const val MY_LIST = "마이 리스트"
