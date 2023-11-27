f739c078cb80a1e116e58f925208a1e2dd0018c6
박진성 <plashdof@gmail.com>
2023-11-27 23:04:56 +0900

    #71 feat : Follow UiState, Event 정의

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/follow/FollowViewModel.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/main/follow/FollowViewModel.kt
@@ -1,11 +1,36 @@
 package com.avengers.nibobnebob.presentation.ui.main.follow
 
 import androidx.lifecycle.ViewModel
+import com.avengers.nibobnebob.presentation.ui.main.follow.model.UiFollowData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+
+data class FollowUiState(
+    val followingList: List<UiFollowData> = emptyList(),
+    val followList: List<UiFollowData> = emptyList(),
+    val recommendFriend: List<UiFollowData> = emptyList()
+)
+
+sealed class FollowEvents{
+    data object NavigateToFollowSearch: FollowEvents()
+    data class NavigateToFollowDetail(val nickName: String): FollowEvents()
+}
+
 
 @HiltViewModel
 class FollowViewModel @Inject constructor(): ViewModel() {
+
+    private val _uiState = MutableStateFlow(FollowUiState())
+    val uiState:StateFlow<FollowUiState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<FollowEvents>()
+    val events: SharedFlow<FollowEvents> = _events.asSharedFlow()
 
 
 }
