5925cbdebf5fa552d8bd070ce0d9fd735e5863ef
LeeTH916 <thoon916@naver.com>
2023-11-28 17:18:04 +0900

    feat : 내 맛집 리스트 응답 시 isWish 추가 #63, #31

--- be/src/user/user.restaurantList.repository.ts
+++ be/src/user/user.restaurantList.repository.ts
@@ -4,6 +4,7 @@ import { UserRestaurantListEntity } from "./entities/user.restaurantlist.entity"
 import { TokenInfo } from "./user.decorator";
 import { SearchInfoDto } from "src/restaurant/dto/seachInfo.dto";
 import { ReviewInfoEntity } from "src/review/entities/review.entity";
+import { UserWishRestaurantListEntity } from "./entities/user.wishrestaurantlist.entity";
 
 @Injectable()
 export class UserRestaurantListRepository extends Repository<UserRestaurantListEntity> {
@@ -29,6 +30,12 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
             return await this
                 .createQueryBuilder('user_restaurant_lists')
                 .leftJoinAndSelect('user_restaurant_lists.restaurant', 'restaurant')
+                .leftJoin(
+                    UserWishRestaurantListEntity,
+                    "user_wish_list",
+                    "user_wish_list.restaurantId = restaurant.id AND user_wish_list.userId = :userId",
+                    { userId: id }
+                  )
                 .select([
                     'user_restaurant_lists.restaurantId AS restaurant_id',
                     'restaurant.name',
@@ -36,7 +43,8 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
                     'restaurant.address',
                     'restaurant.category',
                     "restaurant.phoneNumber",
-                    "restaurant.reviewCnt"
+                    "restaurant.reviewCnt",
+                    `CASE WHEN user_wish_list.userId IS NOT NULL THEN TRUE ELSE FALSE END AS "isWish"`
                 ])
                 .where(`user_restaurant_lists.user_id = :userId and ST_DistanceSphere(
                 location, 
@@ -48,6 +56,12 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
             return await this
                 .createQueryBuilder('user_restaurant_lists')
                 .leftJoinAndSelect('user_restaurant_lists.restaurant', 'restaurant')
+                .leftJoin(
+                    UserWishRestaurantListEntity,
+                    "user_wish_list",
+                    "user_wish_list.restaurantId = restaurant.id AND user_wish_list.userId = :userId",
+                    { userId: id }
+                  )
                 .select([
                     'user_restaurant_lists.restaurantId AS restaurant_id',
                     'restaurant.name',
@@ -55,7 +69,8 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
                     'restaurant.address',
                     'restaurant.category',
                     "restaurant.phoneNumber",
-                    "restaurant.reviewCnt"
+                    "restaurant.reviewCnt",
+                    `CASE WHEN user_wish_list.userId IS NOT NULL THEN TRUE ELSE FALSE END AS "isWish"`
                 ])
                 .where('user_restaurant_lists.user_id = :userId  and user_restaurant_lists.deleted_at IS NULL', { userId: id })
                 .getRawMany();
