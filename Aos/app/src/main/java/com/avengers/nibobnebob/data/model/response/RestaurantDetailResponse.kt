dc61f479713a86732cba61f18ef4d4136208b9d4
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-11-29 18:36:14 +0900

    #104 feat : 맛집 상세 위시 추가 data layer 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/data/model/response/RestaurantDetailResponse.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/model/response/RestaurantDetailResponse.kt
@@ -1,12 +1,28 @@
 package com.avengers.nibobnebob.data.model.response
 
+import com.google.gson.annotations.SerializedName
+
 data class RestaurantDetailResponse(
-    val address: String,
-    val category: String,
-    val id: Int,
-    val location: Location,
-    val name: String,
-    val phoneNumber: String,
-    val reviewCnt: Int
-    //TODO : isWish받아와야함
+    @SerializedName("isMy") val isMy : Boolean,
+    @SerializedName("isWish") val isWish : Boolean,
+    @SerializedName("restaurant_address")val address: String,
+    @SerializedName("restaurant_category")val category: String,
+    @SerializedName("restaurant_id")val id: Int,
+    @SerializedName("restaurant_location")val location: Location,
+    @SerializedName("restaurant_name")val name: String,
+    @SerializedName("restaurant_phoneNumber")val phoneNumber: String,
+    @SerializedName("restaurant_reviewCnt")val reviewCnt: Int,
+    @SerializedName("reviews") val reviews : List<Reviews>?
+)
+
+data class Reviews(
+    @SerializedName("review_id")val id: Int,
+    @SerializedName("review_isCarVisit")val isCarVisit: Boolean,
+    @SerializedName("review_overallExperience")val overallExperience: String,
+    @SerializedName("review_parkingArea")val parkingArea: Int,
+    @SerializedName("review_restroomCleanliness")val restroomCleanliness: Int,
+    @SerializedName("review_service")val service: Int,
+    @SerializedName("review_taste")val taste: Int,
+    @SerializedName("review_transportationAccessibility")val transportationAccessibility: Int,
+    @SerializedName("reviewer") val reviewer : String,
 )
