765f29d1f6055840b2cea244410fe1d19be8b6b3
LeeTH916 <thoon916@naver.com>
2023-11-30 15:14:51 +0900

    feat : object storage에 대한 서비스 로직 구현 #35
    - object storage에 파일 업로드 서비스 구현
    - object storage에서 파일 다운로드를 할 수 있는 서명된 URL 응답 구현
    
    Co-authored-by: GeunH <unlxrmsgur@naver.com>

--- be/src/aws/aws.service.ts
+++ be/src/aws/aws.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
-import { awsConfig } from 'objectStorage.config';
+import { Injectable } from "@nestjs/common";
+import * as AWS from "aws-sdk";
+import { awsConfig } from "objectStorage.config";
+import { v4 } from "uuid";
 
 @Injectable()
 export class AwsService {
-    private s3: AWS.S3;
+  private s3: AWS.S3;
 
-    constructor() {
-        this.s3 = new AWS.S3({
-            endpoint: awsConfig.endpoint,
-            region: awsConfig.region,
-            credentials: {
-                accessKeyId: awsConfig.accessKey,
-                secretAccessKey: awsConfig.secretKey
-            },
-        });
-    }
+  constructor() {
+    this.s3 = new AWS.S3({
+      endpoint: awsConfig.endpoint,
+      region: awsConfig.region,
+      credentials: {
+        accessKeyId: awsConfig.accessKey,
+        secretAccessKey: awsConfig.secretKey,
+      },
+    });
+  }
+
+  uploadToS3(path: string, data: Buffer): string {
+    const uuid = v4();
+
+    this.s3.putObject({
+      Bucket: awsConfig.bucket,
+      Key: `${path}/${uuid}.png`,
+      Body: data,
+    });
+
+    return uuid;
+  }
+
+  getImageURL(path: string) {
+    const signedUrl = this.s3.getSignedUrl("getObject", {
+      Bucket: awsConfig.bucket,
+      Key: path,
+      Expires: 60,
+    });
+
+    console.log(signedUrl);
+  }
 }
