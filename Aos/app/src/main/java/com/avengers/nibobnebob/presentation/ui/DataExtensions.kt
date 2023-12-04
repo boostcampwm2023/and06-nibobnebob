d1f76987470888119e3868d7d60b624f0e826966
박진성 <plashdof@gmail.com>
2023-12-04 16:13:34 +0900

    #172 feat : uri -> MultiPart 변환 Extension 함수 구현

--- Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/DataExtensions.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/presentation/ui/DataExtensions.kt
@@ -1,6 +1,13 @@
 package com.avengers.nibobnebob.presentation.ui
 
+import android.content.Context
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.annotation.RequiresApi
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -33,4 +40,26 @@ internal fun String.toAgeString(): String {
     } else {
         "60세 이상"
     }
+}
+
+internal fun Uri.toMultiPart(context: Context): MultipartBody.Part {
+    val file = File(getRealPathFromUri(this, context) ?: "")
+    val requestFile = file.asRequestBody("image/jpg".toMediaTypeOrNull())
+    return MultipartBody.Part.createFormData("image", file.name, requestFile)
+}
+
+
+// 절대경로 변환
+private fun getRealPathFromUri(uri: Uri, context: Context): String? {
+    var filePath: String? = null
+    val projection = arrayOf(MediaStore.Images.Media.DATA)
+    val cursor = context.contentResolver.query(uri, projection, null, null, null)
+    cursor?.let {
+        if (it.moveToFirst()) {
+            val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            filePath = it.getString(columnIndex)
+        }
+        it.close()
+    }
+    return filePath
 }
