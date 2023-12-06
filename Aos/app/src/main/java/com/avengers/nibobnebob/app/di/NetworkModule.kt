ec8b022a078dc1fa0175f18ae5d01d6e610f96ac
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-12-06 17:37:19 +0900

    #19 feat : 이미지 사이즈 크기에 따른 타임아웃 시간 늘림
    서버에서 아직 적용x

--- Aos/app/src/main/java/com/avengers/nibobnebob/app/di/NetworkModule.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/app/di/NetworkModule.kt
@@ -27,8 +27,8 @@ object NetworkModule {
     ): OkHttpClient {
 
         return OkHttpClient.Builder()
-            .readTimeout(3000, TimeUnit.MILLISECONDS)
-            .connectTimeout(3000, TimeUnit.MILLISECONDS)
+            .readTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(5000, TimeUnit.MILLISECONDS)
             .addInterceptor(httpLoggingInterceptor)
             .addNetworkInterceptor(accessTokenInterceptor)
             .addInterceptor(bearerInterceptor)
