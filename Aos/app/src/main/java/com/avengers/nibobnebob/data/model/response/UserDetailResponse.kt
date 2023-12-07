da4c60f901a8c2c02d2ddf4ac85be0e0144ed461
박진성 <plashdof@gmail.com>
2023-12-07 23:12:37 +0900

    #86 feat : 유저상세 response 수정

--- Aos/app/src/main/java/com/avengers/nibobnebob/data/model/response/UserDetailResponse.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/data/model/response/UserDetailResponse.kt
@@ -2,8 +2,10 @@ package com.avengers.nibobnebob.data.model.response
 
 import com.avengers.nibobnebob.data.model.base.BaseDataModel
 import com.avengers.nibobnebob.data.model.mapper.DomainMapper
-import com.avengers.nibobnebob.data.model.response.SearchRestaurantResponse.Companion.toDomainModel
+import com.avengers.nibobnebob.data.model.response.UserDetailRestaurantResponse.Companion.toDomainModel
 import com.avengers.nibobnebob.domain.model.UserDetailData
+import com.avengers.nibobnebob.domain.model.UserDetailRestaurantData
+import com.google.gson.annotations.SerializedName
 
 data class UserDetailResponse(
     val nickName: String,
@@ -11,7 +13,7 @@ data class UserDetailResponse(
     val region: String,
     val isMale: Boolean,
     val isFollow: Boolean,
-    val restaurants: List<SearchRestaurantResponse>
+    val restaurants: List<UserDetailRestaurantResponse>
 ) : BaseDataModel {
     companion object : DomainMapper<UserDetailResponse, UserDetailData> {
         override fun UserDetailResponse.toDomainModel(): UserDetailData = UserDetailData(
@@ -22,6 +24,32 @@ data class UserDetailResponse(
             isFollow = isFollow,
             restaurants = restaurants.map { it.toDomainModel() }
         )
+    }
+}
+
+
+data class UserDetailRestaurantResponse(
+    @SerializedName("restaurant_id") val id: Int,
+    @SerializedName("restaurant_name") val name: String,
+    @SerializedName("restaurant_location") val location: Location,
+    @SerializedName("restaurant_address") val address: String,
+    @SerializedName("restaurant_phoneNumber") val phoneNumber: String,
+    @SerializedName("restaurant_reviewCnt") val reviewCnt: Int,
+    @SerializedName("restaurant_category") val category: String,
+    @SerializedName("isMy") val isMy: Boolean
+) : BaseDataModel {
+    companion object : DomainMapper<UserDetailRestaurantResponse, UserDetailRestaurantData> {
+        override fun UserDetailRestaurantResponse.toDomainModel(): UserDetailRestaurantData =
+            UserDetailRestaurantData(
+                id = id,
+                name = name,
+                location = location,
+                address = address,
+                phoneNumber = phoneNumber,
+                reviewCnt = reviewCnt,
+                category = category,
+                isMy = isMy
+            )
     }
 }
 
--- Aos/app/src/main/java/com/avengers/nibobnebob/domain/model/UserDetailData.kt
+++ Aos/app/src/main/java/com/avengers/nibobnebob/domain/model/UserDetailData.kt
@@ -1,6 +1,8 @@
 package com.avengers.nibobnebob.domain.model
 
+import com.avengers.nibobnebob.data.model.response.Location
 import com.avengers.nibobnebob.domain.model.base.BaseDomainModel
+import com.google.gson.annotations.SerializedName
 
 data class UserDetailData(
     val nickName: String,
@@ -8,5 +10,17 @@ data class UserDetailData(
     val region: String,
     val isMale: Boolean,
     val isFollow: Boolean,
-    val restaurants: List<SearchRestaurantData>
+    val restaurants: List<UserDetailRestaurantData>
+): BaseDomainModel
+
+
+data class UserDetailRestaurantData(
+    val id: Int,
+    val name: String,
+    val location: Location,
+    val address: String,
+    val phoneNumber: String,
+    val reviewCnt: Int,
+    val category: String,
+    val isMy: Boolean
 ): BaseDomainModel
