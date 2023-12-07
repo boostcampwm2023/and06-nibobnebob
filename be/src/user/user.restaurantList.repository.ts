27e7aebb037fa4595720b84ec56902ae32a477ee
GeunH <unlxrmsgur@naver.com>
2023-12-07 10:57:43 +0900

    fix : 음식 추천시 응답 에러 해결 #64

--- be/src/user/user.restaurantList.repository.ts
+++ be/src/user/user.restaurantList.repository.ts
@@ -109,50 +109,50 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
       return items
     } else {
       let query = this.createQueryBuilder("user_restaurant_lists")
-      .leftJoinAndSelect("user_restaurant_lists.restaurant", "restaurant")
-      .leftJoin(
-        UserWishRestaurantListEntity,
-        "user_wish_list",
-        "user_wish_list.restaurantId = restaurant.id AND user_wish_list.userId = :userId",
-        { userId: id }
-      )
-      .select([
-        "user_restaurant_lists.restaurantId AS restaurant_id",
-        "restaurant.name",
-        "restaurant.location",
-        "restaurant.address",
-        "restaurant.category",
-        "restaurant.phoneNumber",
-        "restaurant.reviewCnt",
-        "user_restaurant_lists.created_at",
-        `CASE WHEN user_wish_list.userId IS NOT NULL THEN TRUE ELSE FALSE END AS "isWish"`,
-      ])
-      .where(
-        "user_restaurant_lists.user_id = :userId and user_restaurant_lists.deleted_at IS NULL",
-        { userId: id }
-      );
-    
-    if (sortInfoDto.sort === 'TIME_ASC') {
-      query = query.orderBy("user_restaurant_lists.created_at", "ASC");
-    } else {
-      query = query.orderBy("user_restaurant_lists.created_at", "DESC");
-    }
-    sortInfoDto.page = parseInt(sortInfoDto.page as unknown as string) || 1;
-    sortInfoDto.limit = parseInt(sortInfoDto.limit as unknown as string) || 10;
+        .leftJoinAndSelect("user_restaurant_lists.restaurant", "restaurant")
+        .leftJoin(
+          UserWishRestaurantListEntity,
+          "user_wish_list",
+          "user_wish_list.restaurantId = restaurant.id AND user_wish_list.userId = :userId",
+          { userId: id }
+        )
+        .select([
+          "user_restaurant_lists.restaurantId AS restaurant_id",
+          "restaurant.name",
+          "restaurant.location",
+          "restaurant.address",
+          "restaurant.category",
+          "restaurant.phoneNumber",
+          "restaurant.reviewCnt",
+          "user_restaurant_lists.created_at",
+          `CASE WHEN user_wish_list.userId IS NOT NULL THEN TRUE ELSE FALSE END AS "isWish"`,
+        ])
+        .where(
+          "user_restaurant_lists.user_id = :userId and user_restaurant_lists.deleted_at IS NULL",
+          { userId: id }
+        );
 
-    const offset = (sortInfoDto.page - 1) * sortInfoDto.limit;
-    query = query.offset(offset).limit(sortInfoDto.limit + 1);
-  
-    const items = await query.getRawMany();
-  
-    const hasNext = items.length > sortInfoDto.limit;
-    const resultItems = hasNext ? items.slice(0, -1) : items;
-  
-    return {
-      hasNext,
-      items : resultItems,
-    }
-  
+      if (sortInfoDto.sort === 'TIME_ASC') {
+        query = query.orderBy("user_restaurant_lists.created_at", "ASC");
+      } else {
+        query = query.orderBy("user_restaurant_lists.created_at", "DESC");
+      }
+      sortInfoDto.page = parseInt(sortInfoDto.page as unknown as string) || 1;
+      sortInfoDto.limit = parseInt(sortInfoDto.limit as unknown as string) || 10;
+
+      const offset = (sortInfoDto.page - 1) * sortInfoDto.limit;
+      query = query.offset(offset).limit(sortInfoDto.limit + 1);
+
+      const items = await query.getRawMany();
+
+      const hasNext = items.length > sortInfoDto.limit;
+      const resultItems = hasNext ? items.slice(0, -1) : items;
+
+      return {
+        hasNext,
+        items: resultItems,
+      }
+
     }
   }
   async getMyFavoriteFoodCategory(id: TokenInfo['id'], region) {
@@ -163,6 +163,7 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
       .where("userRestaurantList.userId = :id", { id })
       .groupBy("restaurant.category")
       .getRawMany();
+
 
     if (categoryCounts.length) {
       const favoriteCategory = categoryCounts.reduce((a, b) => a.count > b.count ? a : b).category;
@@ -226,5 +227,6 @@ export class UserRestaurantListRepository extends Repository<UserRestaurantListE
         return recommendedRestaurants;
       }
     }
+    return [];
   }
 }
